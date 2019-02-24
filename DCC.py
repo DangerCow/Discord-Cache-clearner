b21135fa940e9820bd6633f9bcfe4b003295112f
DangerCow <38820244+DangerCow@users.noreply.github.com>
2019-02-23 22:48:53 -0700

    update 0.7.5
    byts of data deleted

--- DCC.py
+++ DCC.py
@@ -4,6 +4,7 @@ print(os.path.dirname(os.path.realpath(__file__)))
 
 installed = input("have you installed discord to a custom folder? y/n: ")
 
+
 if installed == "y":
     dir = input("directory: ")
 elif installed == "n":
@@ -12,17 +13,20 @@ elif installed == "n":
     dir = start + "\AppData\Roaming\Discord\Cache"
 
 i = 0
+size = 0
 
 for the_file in os.listdir(dir):
     file_path = os.path.join(dir, the_file)
     try:
+        tmpsize = os.path.getsize(file_path)
         if os.path.isfile(file_path):
             os.unlink(file_path)
             print("file deleted " + the_file)
         #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        size += tmpsize
         i += 1
     except Exception as e:
         print("failed to delete file " + the_file)
         
 
-input("deleted " + str(i) + " files")
+input("deleted " + str(i) + " files and cleared " + "{:,}".format(size) + " bytes of storage")

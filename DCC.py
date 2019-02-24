79403432bd8a8630f25a1aa2bc9b90047106259f
DangerCow <38820244+DangerCow@users.noreply.github.com>
2019-02-23 18:14:28 -0700

    Initial commit

---
+++ DCC.py
@@ -0,0 +1,21 @@
+import os
+
+print(os.path.dirname(os.path.realpath(__file__)))
+
+installed = input("have you installed discord with a coutsom folder? y/n: ")
+
+if installed == "y":
+    dir = input("directory: ")
+elif installed == "n":
+    dir = os.path.dirname(os.path.realpath(__file__))
+    start = dir.split("\\")[0] + "\\" + dir.split("\\")[1] + "\\" + dir.split("\\")[2]
+    dir = start + "\AppData\Roaming\Discord\Cache"
+
+for the_file in os.listdir(dir):
+    file_path = os.path.join(dir, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception as e:
+        print(e)
